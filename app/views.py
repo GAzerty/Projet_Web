e209f7ffb3f44e63737545a7828c505d1cb6b517
GAzerty <guilherme.t14@gmail.com>
2019-05-09 23:23:21 +0200

    Ecriture CRUD de Joueur

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
-from app.forms import SignupJoueurForm
+from app.forms import SignupJoueurForm, UpdateJoueurForm
 from django.contrib.auth.models import User
 from app.models import Joueur, Quartier
 
@@ -39,18 +39,59 @@ def signupJoueur(request):
             new_Joueur.save()
             login(request,user)
             return render(request, 'index.html')
+        else:
+            return render(request, "signup_joueur.html", {'SignupJoueurForm': form})
     else:
         form = SignupJoueurForm()
         return render(request, "signup_joueur.html", {'SignupJoueurForm': form})
 
 
-#READ
+#READ - MON COMPTE
+
+#Retourne un Joueur à partir de l'utilisateur connecté
+def getJoueurConnecte(request):
+    utilisateur = User.objects.get(username=request.user.username)  # Je récupère l'utilisateur connecté
+    joueur = Joueur.objects.get(idJoueur=utilisateur)  # Je récupère le joueur correspondant à l'utilisateur
+    return joueur
+
+#@login_required
+def moncompteJoueur(request):
+    joueur = getJoueurConnecte(request)
+    return render(request, "joueur/account.html", {'Joueur': joueur}) #Je renvoie le joueur à la template
 
 
 #UPDATE
-
+def updateJoueur(request):
+    utilisateur = User.objects.get(username=request.user.username)
+    joueur = Joueur.objects.get(idJoueur=utilisateur)
+    if request.method == "POST":
+        form = UpdateJoueurForm(request.POST)
+        if form.is_valid():
+            utilisateur.first_name = form.cleaned_data['first_name']
+            utilisateur.last_name = form.cleaned_data['last_name']
+            utilisateur.email = form.cleaned_data['email']
+            utilisateur.save() #Sauvegarde de l'utilisateur avec ses nouvelles valeurs
+            quartier = Quartier.objects.get(nomQuartier=form.cleaned_data['choix_quartier'])
+            joueur.quartierJoueur = quartier
+            joueur.save() #Sauvegarde du joueur avec ses nouvelles valeurs
+            return moncompteJoueur(request)
+        else:
+            return render(request, "joueur/update_joueur.html", {'UpdateJoueurForm': form,'Joueur': joueur})
+    else:
+        dataform = {
+            'first_name': utilisateur.first_name,
+            'last_name': utilisateur.last_name,
+            'email': utilisateur.email,
+            'choix_quartier': joueur.quartierJoueur,
+        }
+        form = UpdateJoueurForm(dataform) #On lies les données de dataform avec le formulaire, les champs se remplissent en fonctions du nom des champs correspondant au formulaire
+        return render(request, "joueur/update_joueur.html", {'UpdateJoueurForm': form,'Joueur': joueur})
 
 #DELETE
+def deleteJoueur(request):
+    getJoueurConnecte(request).delete()
+    utilisateur = User.objects.get(username=request.user.username).delete
+    return render(request, "index.html")
 
 
 
