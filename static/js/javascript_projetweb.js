020f3d423fe2daa0c9b2cff3bffbbe463c02e2d6
GAzerty <guilherme.t14@gmail.com>
2019-05-10 12:12:52 +0200

    Ecriture de la requete Ajax demande_amis

--- static/js/javascript_projetweb.js
+++ static/js/javascript_projetweb.js
@@ -2,12 +2,20 @@
 
 function demande_amis(){
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/friend/sendto", true);
+    xhttp.open("POST", "/friend/sendto/", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    var inputs = document.getElementsByTagName("input");
 
+    var inputs = document.getElementsByTagName("input");
     var csrf = "csrfmiddlewaretoken="+inputs.csrfmiddlewaretoken.value;
-    var username;  //Recupere la value via getbyID de input
+    var username = document.getElementById('id_demande_usernameAmis').name+"="+document.getElementById('id_demande_usernameAmis').value
     var data = csrf+"&"+username;
+
+    console.log(data)
     xhttp.send(data);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) { //readystate=4 : requête terminé, status=200 : Ok
+            var reponse = JSON.parse(this.response)
+            console.log(reponse.feedback);
+        }
+     };
 }
