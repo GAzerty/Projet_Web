2bb039f1ba9e07ad235d71971375b3f2a46374f8
GAzerty <guilherme.t14@gmail.com>
2019-05-15 23:17:15 +0200

    Push vers Heroku: version stable 1

--- projetweb/settings.py
+++ projetweb/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
-from .secret_settings import *
+#from .secret_settings import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = os.environ.get('SECRET_KEY',True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
