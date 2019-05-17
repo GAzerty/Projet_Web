cd335186b477199b1f7cb2f588de409c330274dc
GAzerty <guilherme.t14@gmail.com>
2019-05-18 01:46:17 +0200

    heroku

--- projetweb/settings.py
+++ projetweb/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',True)
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
